44fd1993ca232cc830e1eb1a9237f0cecd27fd2c
ShumpeiKawamura37 <lrivermht1611flat@icloud.com>
2026-05-29 21:37:53 +0900

    chore: setup

---
+++ backend/study-management-app/src/main/java/com/portfolio/study_management_app/StudyManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.study_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyManagementAppApplication.class, args);
+	}
+
+}
---
+++ backend/study-management-app/src/test/java/com/portfolio/study_management_app/StudyManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.study_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
